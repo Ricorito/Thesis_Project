0572db9b2e0b4e4e72b49f791aa99c76249563c5
Ricorito <71760194+Ricorito@users.noreply.github.com>
2025-07-29 07:15:03 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,9 +20,6 @@ import VerifyRedirect from "./components/authComponents/VerifyRedirect";
 
 import CommunityPostDetail from "./components/communityComponents/CommunityPostDetail";
 
-import LogoPage from "../src/pages/LogoPage"
-
-
 import Layout from "./components/utilityComponents/Layer";
 import "./css/index.css";
 
@@ -45,8 +42,6 @@ function App() {
       <Routes>
         {/* Public routes without layout */}
         <Route path="/" element={<HomePage />} />
-        <Route path="logo" element={<LogoPage />} />
-
         <Route path="/authpage" element={<Authpage />} />
         <Route path="/verify" element={<VerifyRedirect />} />
 
